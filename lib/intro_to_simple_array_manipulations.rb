4c92bcd681806fb054339d755dd847bd88e62423
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-10-12 23:12:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,6 @@
 
 def using_concat(array)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
   @my_favorite_things = array.concat(@my_favorite_things, more_favs)
   p @my_favorite_things
 end 
